6cb7e316e827095742fd97a097f9bea07cb6a198
vanessitaf8 <caryto_z@hotmail.com>
2020-01-15 20:57:32 -0500

    registro creado

--- comdero/src/app/paginas/paginas.module.ts
+++ comdero/src/app/paginas/paginas.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoguinComponent } from './loguin/loguin.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { PrincipalComponent } from './principal/principal.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 @NgModule({
-  declarations: [LoguinComponent, PrincipalComponent],
+  declarations: [LoguinComponent, PrincipalComponent, RegistroComponent],
   imports: [
     CommonModule,
     NgbModule
