80546a2ed7903295c49cb31fe04ea1d6f0ebab78
vanessitaf8 <caryto_z@hotmail.com>
2020-01-20 14:43:24 -0500

    rEGISTRO

--- comdero/src/app/app-routing.module.ts
+++ comdero/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoguinComponent} from './paginas/loguin/loguin.component'
 import {PrincipalComponent} from "./paginas/principal/principal.component";
+import { RegistroComponent } from './paginas/registro/registro.component';
 
 const routes: Routes = [
   {path: 'loguin', component: LoguinComponent},
-  {path:'principal', component:PrincipalComponent}
+  {path:'principal', component:PrincipalComponent},
+  {path: 'registro', component: RegistroComponent},
 ];
 
 @NgModule({
