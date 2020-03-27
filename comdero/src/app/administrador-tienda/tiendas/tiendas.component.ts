3f3492396dfc98c44a28387416dbea849a699642
stteffano13 <tefo.aguayo@gmail.com>
2020-03-27 11:15:38 -0500

    fin disenio catalogo de mis tiendas

--- comdero/src/app/administrador-tienda/tiendas/tiendas.component.ts
+++ comdero/src/app/administrador-tienda/tiendas/tiendas.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tiendas',
   templateUrl: './tiendas.component.html',
   styleUrls: ['./tiendas.component.css']
 })
-export class TiendasComponent implements OnInit {
-
+export class TiendasComponent implements OnInit,AfterContentInit {
+public banderMensaje:boolean=false;
   constructor() { }
 
   ngOnInit() {
   }
+ ngAfterContentInit(): void {
+    this.banderMensaje=true;
+ }
 
 }
