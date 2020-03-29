f8a4cde4f4c8beafee961f05f4828a94a281120f
stteffano13 <tefo.aguayo@gmail.com>
2020-03-29 11:22:14 -0500

    corregido codigo inecesario

--- README.md
+++ README.md
@@ -7,10 +7,15 @@ npm install --save @ng-bootstrap/ng-bootstrap
 
 // mejorar el select 
 
-----incluir esto en el index
+----incluir esto en el index para que el select sea de otro tipo
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.min.css">
 
 
 --------Servicio "api"----------
 npm install nodemailer // correo electronico
+
+
+--- libreria para sidebar
+npm i ng-sidebar
+https://stackblitz.com/edit/ng-sidebar-demo
--- comdero/src/app/administrador-tienda/gestionar-tienda/gestionar-tienda.component.ts
+++ comdero/src/app/administrador-tienda/gestionar-tienda/gestionar-tienda.component.ts
@@ -9,17 +9,9 @@ export class GestionarTiendaComponent  {
   private _opened: boolean = true;
   private _modeNum: number = 1;
   private _positionNum: number = 0;
-  private _dock: boolean = false;
-  private _closeOnClickOutside: boolean = false;
-  private _closeOnClickBackdrop: boolean = false;
-  private _showBackdrop: boolean = false;
   private _animate: boolean = true;
-  private _trapFocus: boolean = true;
-  private _autoFocus: boolean = true;
-  private _keyClose: boolean = false;
   private _autoCollapseHeight: number = null;
   private _autoCollapseWidth: number = 500;
-
   private _MODES: Array<string> = ['over', 'push', 'slide'];
   private _POSITIONS: Array<string> = ['left', 'right', 'top', 'bottom'];
 
@@ -27,61 +19,7 @@ export class GestionarTiendaComponent  {
     this._opened = !this._opened;
   }
 
-  private _toggleMode(): void {
-    this._modeNum++;
 
-    if (this._modeNum === this._MODES.length) {
-      this._modeNum = 0;
-    }
-  }
-
-  private _toggleAutoCollapseHeight(): void {
-    this._autoCollapseHeight = this._autoCollapseHeight ? null : 500;
-  }
-
-  private _toggleAutoCollapseWidth(): void {
-    this._autoCollapseWidth = this._autoCollapseWidth ? null : 500;
-  }
-
-  private _togglePosition(): void {
-    this._positionNum++;
-
-    if (this._positionNum === this._POSITIONS.length) {
-      this._positionNum = 0;
-    }
-  }
-
-  private _toggleDock(): void {
-    this._dock = !this._dock;
-  }
-
-  private _toggleCloseOnClickOutside(): void {
-    this._closeOnClickOutside = !this._closeOnClickOutside;
-  }
-
-  private _toggleCloseOnClickBackdrop(): void {
-    this._closeOnClickBackdrop = !this._closeOnClickBackdrop;
-  }
-
-  private _toggleShowBackdrop(): void {
-    this._showBackdrop = !this._showBackdrop;
-  }
-
-  private _toggleAnimate(): void {
-    this._animate = !this._animate;
-  }
-
-  private _toggleTrapFocus(): void {
-    this._trapFocus = !this._trapFocus;
-  }
-
-  private _toggleAutoFocus(): void {
-    this._autoFocus = !this._autoFocus;
-  }
-
-  private _toggleKeyClose(): void {
-    this._keyClose = !this._keyClose;
-  }
 
   private _onOpenStart(): void {
     console.info('Sidebar opening');
@@ -99,11 +37,5 @@ export class GestionarTiendaComponent  {
     console.info('Sidebar closed');
   }
 
-  private _onTransitionEnd(): void {
-    console.info('Transition ended');
-  }
 
-  private _onBackdropClicked(): void {
-    console.info('Backdrop clicked');
-  }
 }
