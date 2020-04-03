2f50b95d4a57dbbf16d514318c26a8c01d9afecd
stteffano13 <tefo.aguayo@gmail.com>
2020-04-03 14:49:11 -0500

    final video y imagenes con restricciones

--- comdero/src/app/administrador-tienda/gestionar-tiendas/productos/productos.component.ts
+++ comdero/src/app/administrador-tienda/gestionar-tiendas/productos/productos.component.ts
@@ -9,7 +9,14 @@ import {Component, OnInit} from '@angular/core';
 export class ProductosComponent implements OnInit {
 
   public htmlcomponent;
-  public url2="./../assets/images/imagenes-fondo.png"
+  public url2 = "./../assets/images/imagenes-fondo.png";
+  images = [];
+  banderaAgregarImagen: boolean = false;
+  banderaMaximoImagenes: boolean = true;
+  banderaMensajeMaximoImagenes: boolean = false;
+  banderaMensajeMaximoVideo: boolean = false;
+  public data: any = [];
+  public banderaAnimacionVideo: boolean = false;
 
   public editorConfig = {
     "editable": true,
@@ -38,52 +45,66 @@ export class ProductosComponent implements OnInit {
   ngOnInit() {
   }
 
-  images = [];
- banderaAgregarImagen:boolean=false;
   onFileChange(event) {
+
     if (event.target.files && event.target.files[0]) {
       var filesAmount = event.target.files.length;
-      for (let i = 0; i < filesAmount; i++) {
-        var reader = new FileReader();
-
-        reader.onload = (event: any) => {
-          console.log(event.target.result);
-          this.images.push(event.target.result);
-          this.banderaAgregarImagen=true;
-          document.forms["form"].reset();
+      if (filesAmount > 6) {
+        this.banderaMensajeMaximoImagenes = true;
+      } else {
+        for (let i = 0; i < filesAmount; i++) {
+          var reader = new FileReader();
+          reader.onload = (event: any) => {
+            console.log(event.target.result);
+            this.images.push(event.target.result);
+            this.banderaAgregarImagen = true;
+            document.forms["form"].reset();
+            if (this.images.length > 6) {
+              this.images = [];
+              document.forms["form"].reset();
+              this.banderaAgregarImagen = false;
+              this.banderaMensajeMaximoImagenes = true;
+            } else if (this.images.length == 6) {
+              this.banderaMensajeMaximoImagenes = false
+              this.banderaMaximoImagenes = false;
+            }
+          }
+          reader.readAsDataURL(event.target.files[i]);
         }
-
-        reader.readAsDataURL(event.target.files[i]);
       }
     }
   }
 
-  public data: any = [];
 
-  public fileChangeListener($event) {
+  public onVideoChange($event) {
+    this.banderaAnimacionVideo = true;
+    debugger
     let fileList: FileList = $event.target.files;
     this.data = {};
     if (fileList.length > 0) {
       let file: File = fileList[0];
-      console.log('video seleccionado', file);
-      let myReader: FileReader = new FileReader();
-      let that = this;
-      myReader.onloadend = (loadEvent: any) => {
-        console.log('video', myReader.result);
-        this.data.video = myReader.result;
-        this.data.type = file.type;
-      };
-      myReader.readAsDataURL(file);
-
+    /*  console.log('video seleccionado', file);*/
+      if (file.size < 150000000) {
+        this.banderaMensajeMaximoVideo = false
+        let myReader: FileReader = new FileReader();
+        let that = this;
+        myReader.onloadend = (loadEvent: any) => {
+          //  console.log('video', myReader.result);
+          this.data.video = myReader.result;
+          this.data.type = file.type;
+          this.banderaAnimacionVideo = false;
+        };
+        myReader.readAsDataURL(file);
+      } else {
+        this.banderaAnimacionVideo = false;
+        this.banderaMensajeMaximoVideo = true;
+      }
     }
   }
 
-  quitar(images: any) {
-    console.log("as", images, this.images);
+ public quitar(images: any) {
     this.images.splice(images, 1);
-    console.log("cortado", this.images);
     document.forms["form"].reset();
-
   }
 
 }
