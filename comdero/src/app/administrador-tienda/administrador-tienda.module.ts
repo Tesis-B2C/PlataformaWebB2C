5f2c9419d640dfccb9209bf4851765287b4f7f1c
stteffano13 <tefo.aguayo@gmail.com>
2020-04-05 17:06:07 -0500

    fina/l mis tiendas

--- comdero/src/app/administrador-tienda/administrador-tienda.module.ts
+++ comdero/src/app/administrador-tienda/administrador-tienda.module.ts
@@ -5,18 +5,20 @@ import { AdministradorTiendaRoutingModule } from './administrador-tienda-routing
 import { TiendasComponent } from './tiendas/tiendas.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { SidebarModule } from 'ng-sidebar';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 /*import {NgbButtonsModule} from "@ng-bootstrap/ng-bootstrap";*/
 
 @NgModule({
   declarations: [TiendasComponent, EncabezadoComponent,],
-  imports: [
-    CommonModule,
-    AdministradorTiendaRoutingModule,
-    SidebarModule,
+    imports: [
+        CommonModule,
+        AdministradorTiendaRoutingModule,
+        SidebarModule,
+        NgbModule,
 
-   /* NgbButtonsModule,*/
+        /* NgbButtonsModule,*/
 
-  ]
+    ]
 })
 export class AdministradorTiendaModule { }
