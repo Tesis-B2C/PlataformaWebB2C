222a60f0bbe3538dbec416173ed63b2ba1fdc2aa
vanessitaf8 <caryto_z@hotmail.com>
2020-06-26 22:36:35 -0500

    v

--- comdero/src/app/administrador-tienda/gestionar-tiendas/metodos-envio/metodos-envio.component.ts
+++ comdero/src/app/administrador-tienda/gestionar-tiendas/metodos-envio/metodos-envio.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Opcion_Envio} from "../../../modelos/opcion_envio";
-import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-metodos-envio',
@@ -11,16 +11,19 @@ export class MetodosEnvioComponent implements OnInit {
   public disabledRetiroLocal = true;
   public disabledEnvioDomicilio = true;
 
+  public Opcion_EnvioLocal: Opcion_Envio;
   public activarTarifaLocalPeso = false;
   public activarTarifaLocalPrecio = false;
   public vectorTarifasLocal: Array<number> = [1];
 
+  public Opcion_EnvioResto: Opcion_Envio;
+  public activarTarifaRestoPeso = false;
+  public activarTarifaRestoPrecio = false;
   public vectorTarifasResto: Array<number> = [1];
 
-  public Opcion_Envio:Opcion_Envio;
-
-  constructor() {
-    this.Opcion_Envio = new Opcion_Envio(null,null,null,null,null,null,null,null);
+  constructor(private cp: CurrencyPipe) {
+    this.Opcion_EnvioLocal = new Opcion_Envio(null, null, null, null, null, null, null, null);
+    this.Opcion_EnvioResto = new Opcion_Envio(null, null, null, null, null, null, null, null);
   }
 
   ngOnInit() {
@@ -48,26 +51,31 @@ export class MetodosEnvioComponent implements OnInit {
       this.activarTarifaLocalPrecio = false
     }
 
-    if (opcion == 'precio' && event.target.checked)  {
+    if (opcion == 'precio' && event.target.checked) {
       this.activarTarifaLocalPeso = false;
       this.activarTarifaLocalPrecio = true;
     }
   }
 
-  public cancelarTarifaLocal(){
+  public cancelarTarifaLocal() {
     this.activarTarifaLocalPeso = false;
     this.activarTarifaLocalPrecio = false;
   }
 
-  public modal:NgbActiveModal;
-  public agregarTarifaLocal(){
+  public agregarTarifaLocal() {
     this.vectorTarifasLocal.push(1);
-    this.modal.close();
   }
 
-  public eliminarTarifaLocal(indice){
-    this.vectorTarifasLocal.splice(indice,1);
+  public eliminarTarifaLocal(indice) {
+    this.vectorTarifasLocal.splice(indice, 1);
   }
 
+  public transformar(element: any) {
+    debugger;
+    let valor = this.cp.transform(element.target.value, '$',);
+    //let alter=formatCurrency(element.target.value,'USD',getCurrencySymbol('USD', 'wide'));
+    let valor2 = valor.split("$")
+    element.target.value = valor2[1].replace(',', "");
+  }
 
 }
