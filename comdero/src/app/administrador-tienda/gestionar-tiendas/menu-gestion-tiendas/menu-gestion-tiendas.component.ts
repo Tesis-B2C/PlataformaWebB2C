06bb38cdf918b0604e235d4e5edb88eacd55d57b
stteffano13 <tefo.aguayo@gmail.com>
2020-07-01 10:17:09 -0500

    fianl rutas

--- comdero/src/app/administrador-tienda/gestionar-tiendas/menu-gestion-tiendas/menu-gestion-tiendas.component.ts
+++ comdero/src/app/administrador-tienda/gestionar-tiendas/menu-gestion-tiendas/menu-gestion-tiendas.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-menu-gestion-tiendas',
   templateUrl: './menu-gestion-tiendas.component.html',
@@ -7,10 +7,11 @@ import {Component} from '@angular/core';
 })
 export class MenuGestionTiendasComponent {
 public banderaSideBar:boolean=false;
-  constructor() {
+  constructor(private router: Router) {
   }
   abrirSideBar(){
     this.banderaSideBar=!this.banderaSideBar;
   }
+
 
 }
