bd4678d0527334cecd6ba853627b9e1026479945
vanessitaf8 <caryto_z@hotmail.com>
2020-07-24 11:35:12 -0500

    principal

--- comdero/src/app/principales/principal/principal.component.ts
+++ comdero/src/app/principales/principal/principal.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CategoriaServicio} from "../../servicios/categoria.servicio";
 import {TiendaServicio} from "../../servicios/tienda.servicio";
 import {AgenteServicio} from "../../servicios/agente.servicio";
 import {ActivatedRoute, Router} from "@angular/router";
+import {NgbCarouselConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-principal',
@@ -12,10 +13,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class PrincipalComponent implements OnInit {
   /*images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/2000/400`);*/
   images = ["../../../assets/estilos-principal/images/bannerPrincipal.png",];
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
 
   // tslint:disable-next-line: max-line-length
   public categorias;
-  public c1=[];
+  public c1 = [];
   public c2;
   public c3;
   public vectorIconos = ['fa fa-charging-station', 'fa fa-tshirt',
@@ -25,10 +28,12 @@ export class PrincipalComponent implements OnInit {
     'fa fa-dog', 'fa fa-gamepad', 'fa fa-grin-stars', 'fa fa-heartbeat', 'fa fa-building', 'fa fa-tractor'];
 
   public productosObtenidos;
-  public product = [1, 1, 9,8,4,5,5,4,3,3,4,5,6,3,4,5];
+  public product = [1, 1, 9, 8, 4, 5, 5, 4, 3, 3, 4, 5, 6, 3, 4, 5];
 
-  constructor(private route: ActivatedRoute, private router: Router,private _agenteServicio: AgenteServicio,private _tiendaServicio:TiendaServicio, private _categoriaServicio: CategoriaServicio) {
-
+  constructor(private route: ActivatedRoute, private router: Router, private _agenteServicio: AgenteServicio, private _tiendaServicio: TiendaServicio, private _categoriaServicio: CategoriaServicio, config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
   }
 
   ngOnInit() {
@@ -38,7 +43,7 @@ export class PrincipalComponent implements OnInit {
 
   public async obtenerTodosProductos() {
     let response = await this._tiendaServicio.obtenerTodosProductos().toPromise();
-    console.log("PRODUCTOS"+JSON.stringify(response));
+    console.log("PRODUCTOS" + JSON.stringify(response));
     this.productosObtenidos = response.data;
     this.productosObtenidos.forEach(elemnt => {
       console.log(elemnt.PRODUCTO.NOMBRE_PRODUCTO);
@@ -64,17 +69,17 @@ export class PrincipalComponent implements OnInit {
     }
   }
 
-   public async vender(){
-    try{
-     let identidad = this._agenteServicio.getIdentity();
-     let response = await this._tiendaServicio.getMisTiendas(identidad.COD_AGENTE).toPromise();
-       if(response.data){
-         this.router.navigate(['/administrador/administrador-tienda/mis-tiendas']);
-       }
-    }catch (e) {
+  public async vender() {
+    try {
+      let identidad = this._agenteServicio.getIdentity();
+      let response = await this._tiendaServicio.getMisTiendas(identidad.COD_AGENTE).toPromise();
+      if (response.data) {
+        this.router.navigate(['/administrador/administrador-tienda/mis-tiendas']);
+      }
+    } catch (e) {
       this.router.navigate(['/registro-tienda']);
       console.log("error:" + JSON.stringify((e).error.message));
     }
     // [routerLink]="['/registro-tienda']"
-   }
+  }
 }
