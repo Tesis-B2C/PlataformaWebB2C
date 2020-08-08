91d7b22853ec85401e58040a945e14a98a524d54
stteffano13 <tefo.aguayo@gmail.com>
2020-08-07 21:10:49 -0500

    creado GuardRouting

--- comdero/src/app/app-routing.module.ts
+++ comdero/src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { RegistroComponent } from './paginas/registro/registro.component';
 import { RegistroTiendaComponent } from './paginas/registro-tienda/registro-tienda.component';
 import {OlvidoContraseniaComponent} from './paginas/olvido-contrasenia/olvido-contrasenia.component';
 import { OlvidoContraseniaPaso2Component } from './paginas/olvido-contrasenia-paso2/olvido-contrasenia-paso2.component';
+import {AuthGuard} from "./shared/guards/auth.guard";
 
 const routes: Routes = [
 
   {path: 'principales', loadChildren:'./principales/principales.module#PrincipalesModule'},
   {path: 'administrador', loadChildren:'./administrador-tienda/administrador-tienda.module#AdministradorTiendaModule'},
   {path: 'loguin/:token', component: LoguinComponent},
-  {path: 'loguin', component: LoguinComponent},
+  {path: 'loguin', component: LoguinComponent,canActivate:[AuthGuard]},
  // {path:'principal', component:PrincipalComponent},
   {path: 'registro', component: RegistroComponent},
   {path: 'registro-tienda', component: RegistroTiendaComponent},
