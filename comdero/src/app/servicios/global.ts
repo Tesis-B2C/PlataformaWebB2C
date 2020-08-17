74e2157b441cd6d67b8d95e463102a0540f69b38
vanessitaf8 <caryto_z@hotmail.com>
2020-08-17 17:27:14 -0500

    CAMBIOS UNIDOS

--- comdero/src/app/servicios/global.ts
+++ comdero/src/app/servicios/global.ts
@@ -1,7 +1,8 @@
 export var GLOBAL = {
-  //url: "http://localhost:3977/api/",
+  url: "http://localhost:3977/api/",
+  urlImagen: "http://localhost:3977/"
   //url: "http://www.dogipaseoscaninos.com:3977/api/"
   //url:"http://192.168.1.8:3977/api/"
-  url: "http://192.168.0.104:3977/api/",
-  urlImagen: "http://192.168.0.104:3977/"
+  //url: "http://192.168.0.104:3977/api/",
+  //urlImagen: "http://192.168.0.104:3977/"
 };
