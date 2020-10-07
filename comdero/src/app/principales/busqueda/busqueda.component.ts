0b200136202edeb803360288162dbee485fadad0
stteffano13 <tefo.aguayo@gmail.com>
2020-10-07 08:23:52 -0500

    listo categorias en busqueda

--- comdero/src/app/principales/busqueda/busqueda.component.ts
+++ comdero/src/app/principales/busqueda/busqueda.component.ts
@@ -34,8 +34,9 @@ export class BusquedaComponent implements OnInit, OnDestroy, OnChanges {
   currentRate = 1;
   public categorias;
   public c1 = [];
-  public c2;
-  public c3;
+  public c2=[];
+  public c3=[];
+  public bandera:boolean=true;
   public vectorIconos = ['fa fa-charging-station', 'fa fa-tshirt',
     'fa fa-ring', 'fa fa-baby-carriage', 'fa fa-home',
     'fa fa-gem', 'fa fa-palette', 'fa fa-laptop',
@@ -71,30 +72,49 @@ export class BusquedaComponent implements OnInit, OnDestroy, OnChanges {
     delete this.datosObtenidos;
     delete this.vectorTienda;
   }
-
   public async getCategorias() {
     try {
       let response = await this._categoriaServicio.getCategorias().toPromise();
+
       this.categorias = response.data;
+
       this.categorias.forEach(elemnt => {
         if (elemnt.TIPO == 'C1') {
           this.c1.push(elemnt)
-        } /*else if (elemnt.TIPO == 'C2') {
+        } else if (elemnt.TIPO == 'C2') {
           this.c2.push(elemnt)
         } else if (elemnt.TIPO == 'C3') {
           this.c3.push(elemnt)
-        }*/
+        }
       })
+
+
     } catch (e) {
-      if (!(e instanceof HttpErrorResponse)){
-        console.log("error Parseado:" +typeof(e)+ JSON.stringify(e));
-        console.log("error como objeto:"+ e);
+      if (!(e instanceof HttpErrorResponse)) {
+        console.log("error Parseado:" + typeof (e) + JSON.stringify(e));
+        console.log("error como objeto:" + e);
         if (JSON.stringify(e) === '{}')
           this.mensageError(e);
         else this.mensageError(JSON.stringify(e));
       }
     }
+
   }
+  verificar(codigo, nombre) {
+    let bandera: boolean = false;
+    this.categorias.forEach(elemnt => {
+      if (elemnt.CAT_ID_CATEGORIA == codigo) {
+        bandera = true
+      }
+    });
+
+    if (!bandera) {
+      this.router.navigate(['/principales/menu/busqueda-categoria', codigo, nombre])
+
+    }
+
+  }
+
 
   public noExite = 'assets/images/no-image.png';
 
