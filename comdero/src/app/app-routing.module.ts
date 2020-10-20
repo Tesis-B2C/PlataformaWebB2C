a17155bff974f2a64811da2c08b10f29f1a2cafe
stteffano13 <tefo.aguayo@gmail.com>
2020-10-20 18:13:34 -0500

    listo corregido rutas

--- comdero/src/app/app-routing.module.ts
+++ comdero/src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation:'reload'})],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
